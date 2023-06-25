372084dcadf4dc42c34e20699b1e098e541f5268
tappei.miyako <miyako0901t@gmail.com>
2023-06-25 21:14:37 +0900

    add mysql driver

---
+++ app/src/main/kotlin/com/example/driver/userDriver.kt
@@ -0,0 +1,25 @@
+package com.example.driver
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+
+class UserDriver {
+    object User : IntIdTable("user") {
+        val name = varchar("name", 255)
+    }
+
+    fun connection(): String? {
+        Database.connect("jdbc:mysql://127.0.0.1:3306/user", driver = "com.mysql.jdbc.Driver", user = "root", password = "root")
+
+        var user: ResultRow? = null
+        transaction {
+            // Query the user with the given ID
+            user = User.select { User.id eq 1 }.singleOrNull()
+        }
+        return user?.let { it[User.name] }
+    }
+
+}
--- app/src/main/kotlin/com/example/plugins/Routing.kt
+++ app/src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.example.plugins
 
+import com.example.driver.UserDriver
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -7,7 +8,9 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Hello World!\n")
+            val driver = UserDriver()
+            val name = driver.connection()
+            call.respondText("Hello ${name}!\n")
         }
         get("/v1/systems/ping") {
             call.respondText("pong\n")
