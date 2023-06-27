1e0e283be848aa2e0570c319559c7caef6fc0e4f
tappei.miyako <miyako0901t@gmail.com>
2023-06-28 08:41:42 +0900

    WIP

--- app/src/main/kotlin/com/example/driver/UserDriver.kt
+++ app/src/main/kotlin/com/example/driver/UserDriver.kt
@@ -5,13 +5,16 @@ import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.time.LocalDateTime
+import org.jetbrains.exposed.sql.kotlin.datetime.datetime
 
 class UserDriver {
     object User : IntIdTable("user") {
         val name = varchar("name", 255)
+        val createdAt = datetime("created_at").default(LocalDateTime.now())
     }
 
-    fun connection(id: Int): String? {
+    fun connection(id: Int) {
         Database.connect("jdbc:mysql://127.0.0.1:3306/user", driver = "com.mysql.jdbc.Driver", user = "root", password = "root")
 
         var user: ResultRow? = null
@@ -19,7 +22,7 @@ class UserDriver {
             // Query the user with the given ID
             user = User.select { User.id eq id }.singleOrNull()
         }
-        return user?.let { it[User.name] }
+        return user
     }
 
 }
